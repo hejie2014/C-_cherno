a4d1476692533aaeb80d652f69f47914ad9980bf
hejie2014 <2014119hu@gmail.com>
2025-11-24 23:10:51 +0800

    重载运算符opertaor

--- 类的知识_运算符_转换函数/counter/counter.h
+++ 类的知识_运算符_转换函数/counter/counter.h
@@ -27,6 +27,59 @@ public:
 	{
 		return count;
 	}
+	//重载输出运算符
+	// 判断计数是否不相等
+	bool operator!= (const Counter& other) const
+	{
+		return this->count != other.count;
+	}
+	// 判断计数是否相等
+	bool operator== (const Counter& other) const
+	{
+		return this->count == other.count;
+	}
+	// 判断计数是否为零
+	bool operator!() const
+	{
+		return this->count == 0;
+	}
+
+	Counter& operator++() // 前置++
+	{
+		increment();
+		// if(count < std::numeric_limits<unsigned int>::max()) count++;
+
+		return *this;
+	}
+	Counter operator++(int) // 后置++
+	{
+		Counter temp = *this;
+		++(*this);
+		// if(count < std::numeric_limits<unsigned int>::max()) count++;
+		return temp;
+	}
+	Counter& operator--() // 前置--
+	{
+		decrement();
+		// if(count > 0) count--;
+		return *this;
+	}
+	Counter operator--(int) // 后置--
+	{
+		Counter temp = *this;
+		--(*this);
+		// if(count < std::numeric_limits<unsigned int>::max()) count++;
+		return temp;
+	}
+	/*
+	 unsigned x;
+	 //获取计数值
+	 Counter cnt;
+	 x = unsigned(cnt);
+	 x = cnt;
+	 x = (unsigned)cnt;
+	 X = static_cast<unsigned>(cnt);
+	*/
 	/*unsigned int getCount() const
 	{
 		return count;
--- 类的知识_运算符_转换函数/counter/counterTest.cpp
+++ 类的知识_运算符_转换函数/counter/counterTest.cpp
@@ -3,24 +3,60 @@
 
 using namespace std;
 
+
 int main()
 {
 	int number;
 	Counter x;
-	cout << "当前的计数值： " << x << endl;
-
+	Counter y;
+	cout << "当前的计数值： " << static_cast<unsigned int>(x) << endl;
 	cout << "增加计数的次数： ";
 	cin >> number;
 	for (int i = 0; i < number; i++)
 	{
-		x.increment();
-		cout << x << endl;
+		cout << ++x << ' ' << y++ << endl;
 	}
+	cout << "x=" << x << ", y=" << y << endl;
 	cout << "减少计数的次数： ";
 	cin >> number;
 	for (int i = 0; i < number; i++)
 	{
-		x.decrement();
-		cout << x<< endl;
+		cout << --x << ' ' << y-- << endl;
 	}
+	cout << "x=" << x << ", y=" << y << endl;
 }
+
+//当前的计数值： 0
+//增加计数的次数： 4
+//1 0
+//2 1
+//3 2
+//4 3
+//x = 4, y = 4
+//减少计数的次数： 3
+//3 4
+//2 3
+//1 2
+//x = 1, y = 1
+
+//int main()
+//{
+//	int number;
+//	Counter x;
+//	cout << "当前的计数值： " << x.operator unsigned int() << endl;
+//	// x.operator unsigned int()
+//	cout << "增加计数的次数： ";
+//	cin >> number;
+//	for (int i = 0; i < number; i++)
+//	{
+//		x.increment();
+//		cout << x << endl;
+//	}
+//	cout << "减少计数的次数： ";
+//	cin >> number;
+//	for (int i = 0; i < number; i++)
+//	{
+//		x.decrement();
+//		cout << x<< endl;
+//	}
+//}
